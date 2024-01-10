129ec6a890506d7ea2aa1d888f27bc69955537e7
p4trykk <pklytta1@gmail.com>
2024-01-10 18:58:46 +0100

    day11 cd
    day11 part2 cd regex semi-advanced exercises

--- day11.py
+++ day11.py
@@ -36,3 +36,41 @@ phoneNrRegex=re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #obiekt typu Regex
 check2=phoneNrRegex.search('Mój numer telefonu to 746-904-6758') #wywolanie metody tego obiektu do wyszukiwania w łańcuchu znaków zadanego wzorca
 print('Znaleziono numer telefonu: '+check2.group()) #wywołanie metody group() do obiektu Match aby wyświtlić wyszukany wynik
 
+phoneNrGroupRegex=re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+check3=phoneNrGroupRegex.search('Mój numer telefonu to 746-904-6758 i elo')
+print(check3.group(1)) #746
+print(check3.group(2)) #904-6758
+print(check3.group(0)) #746-904-6758 to samo co bez argumentu 
+print(check3.groups()) #('746', '904-6758') wszystkie grupy w krotce
+first, second = check3.groups() #rozdzielenie elementów krotki na 2 zmienne
+print(first)
+print(second)
+phoneNrWithBracketsGroupRegex=re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
+check4=phoneNrWithBracketsGroupRegex.search('Mój numer telefonu w wersji z nawiasami to (746)-904-6758 a NIE 746-904-6758 i elo elo')
+print(check4.group()) #(746)-904-6758
+
+chooseHero=re.compile(r'SpiderMan|Batman|Aquaman')
+text1='Oto lista superbohaterów: Superman, Capitan America, Batman, Ironman, Spiderman i Aquaman. EL0'
+check5=chooseHero.search(text1)
+print(check5.group()) #Znajduje pierwsze wystąpienie z listy w chooseHero, czyli: Batman
+check6=chooseHero.findall(text1)
+print(check6) #findall znajduje wszystkie wystąpienia z obiektu chooseHero i zapisuje do listy
+chooseHeroByMAN=re.compile(r'(Aqua|Bat|Spider|Super|Iron)man')
+check7=chooseHeroByMAN.search('Lista superHERO: man, Batman, Daredevil, Flash, Capitan America, Spiderman, Thor, Ironman')
+print(check7.group()) #wyszukuje tylko pierwsze wystąpienie: Batman
+check8=chooseHeroByMAN.findall(text1)
+print(check8) #['Super', 'Bat', 'Iron', 'Spider', 'Aqua']
+
+WordsEnding=re.compile(r'sejm(ie|u|ów|owi)?')
+check9=WordsEnding.search('W sejmie każdy poseł uczestniczy w głosowaniu.')
+print(check9.group()) #sejmie
+krzyzowka=re.compile(r'woda(nowa)?soda(glowa|choroba)?')
+text2='fimdslkmilkgitaraadnasdawodanowasodaglowanaziretrefwodasodagg'
+check10=krzyzowka.search(text2)
+print(check10.group()) #pierwsze wystąpienie: wodanowasodaglowa
+check11=krzyzowka.findall(text2)
+print(check11) #[('nowa', 'glowa'), ('', '')]
+krzyzowka2=re.compile(r'(ka)*du') # * wielokrotne powtarzanie wyrażenia w nawiasach 
+text3='flugiptyuuggkakakaniszikakadusup'
+check12=krzyzowka2.search(text3)
+print(check12.group()) #kakadu
