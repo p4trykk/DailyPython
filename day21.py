9f8e22abbcc8e2bf45d01f3d048387a385c6219e
p4trykk <128367670+p4trykk@users.noreply.github.com>
2024-01-28 18:58:00 +0100

    day21
    open Google pages automatically by python program

--- day21.py
+++ day21.py
@@ -1,4 +1,4 @@
-import requests, bs4, os, webbrowser
+import requests, bs4, os, webbrowser, sys
 
 res=requests.get('https://nostarch.com/')
 #res.raise_for_status()
@@ -15,3 +15,14 @@ content=readThis.read()
 exampleSoup=bs4.BeautifulSoup(content, 'html.parser')
 elems=exampleSoup.select('#node-745') #wyszukac ten fragment z pliku html
 print('Typ: '+str(type(elems))+' długość: '+str(len(elems))+' identify in html: '+str(elems[0])+ 'text: '+str(elems[0].getText()))
+
+#projekt: wyszukiwanie typ lucky strike
+
+print('Wyszukiwanie...')
+res=requests.get('https://www.google.com/search?q='+''.join(sys.argv[1:]))
+res.raise_for_status()
+soup=bs4.BeautifulSoup(res.text, features='html.parser')
+linkElements=soup.select('.r a')
+numOpen=min(4, len(linkElements))
+for x in range(numOpen):
+    webbrowser.open('http://google.pl'+linkElements[x].get('href'))
